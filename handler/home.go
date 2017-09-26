78dd42e50b6629338985b3017cc071e8b0247af3
mtdx <ms2asdk@gmail.com>
2017-09-26 12:49:50 -0700

    render json

--- handler/home.go
+++ handler/home.go
@@ -1,8 +1,25 @@
 package handler
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/go-chi/render"
+)
+
+type homeResponse struct {
+	Elapsed int64 `json:"elapsed"`
+}
+
+func (rd *homeResponse) Render(w http.ResponseWriter, r *http.Request) error {
+	// Pre-processing before a response is marshalled and sent across the wire
+	rd.Elapsed = 10
+	return nil
+}
 
 // Home route handler
 func Home(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte(""))
+	resp := &homeResponse{}
+
+	render.Status(r, http.StatusOK)
+	render.Render(w, r, resp)
 }
